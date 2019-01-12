335afb67e57bcc9e538369b3aecf72d8f1715895
Christopher Kistner <chris.kistner0@gmail.com>
2019-01-12 10:29:28 -0800

    DB index.js file setup

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const env = process.env.NODE_ENV || 'development'
+
+// getting config for enviroment
+const config = require('../knexfile')[env]
+
+// creating connection to database
+const connection = require('knex')(config)
+
+module.exports = connection
