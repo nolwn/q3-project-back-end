c415efda79ab2dc2607826451c911aa5046ad020
Christopher Kistner <chris.kistner0@gmail.com>
2019-01-23 09:54:10 -0800

    Updated getUser route for use on the front end

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router({mergeParams: true});
 const userController = require('../controllers/users');
+const authController = require('../controllers/auth')
 
 router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers)
-router.get('/:user_id', userController.getUser);
+router.get('/', authController.userAuthenticated, authController.isSelf, userController.getAllUsers)
+router.get('/:user_id', authController.userAuthenticated, authController.isSelf, userController.getUser);
 router.delete('/:user_id', userController.deleteUser);
 
 module.exports = router
